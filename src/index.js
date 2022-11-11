86c3f9ab111e51d806493450a7ca02100d1aa73d
ravimeena <ravim125@hupp.in>
2022-11-11 15:39:57 +0530

    Another commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// git push https://apmeena@github.com/apmeena/amplify-react-graphql.git
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
